0202a9f37cbccd100ad6d36cd5074381e8e83db8
webnafis <shahriar.nafis210@gmail.com>
2025-08-22 11:28:37 +0600

    added production environment file

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  apiBaseLink: 'https://api-client.saleecom.com',
+  ftpBaseLink: 'https://cdn.saleecom.com',
+  ftpPrefixPath: 'https://cdn.saleecom.com/upload/images',
+
+
+};
+
+
